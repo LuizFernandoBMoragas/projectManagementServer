4a86381855f75e905d7cdea6b0a20e436562ff15
Luiz Fernando Barros Moragas <luiz.moragas@hotmail.com>
2022-05-07 22:47:56 +0100

    all the end points has been created

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/read", (req, re) => {});
+app.post("/create", (req, re) => {});
+app.put("/update:id", (req, re) => {});
+app.delete("/delete:id", (req, re) => {});
 
 const port = process.env.PORT || 8080;
 
